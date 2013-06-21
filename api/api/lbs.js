a9e53426b38f59bee1bb4a57a09b147fa452b074
sycs <sycs@qq.com>
2013-06-21 09:52:19 +0800

    test

---
+++ api/api/lbs.js
@@ -0,0 +1,67 @@
+﻿resources.push({
+    name: "LBS",
+    operations: [
+        {
+            notes: "更新地理位置",
+            path: "/lbs/update_pos",
+            http_method: "post",
+            format: "json",
+            description: "此接口主要用于更新当前用户地理位置信息",
+            parameters: [
+                {
+                    parameter: "uid",
+                    value: "562354",
+                    description: "用户唯一ID",
+                    data_type: "string"
+                },
+                {
+                    parameter: "longitude",
+                    value: "30.525565",
+                    description: "经度，例如：30.525565",
+                    data_type: "string"
+                },
+                {
+                    parameter: "latitude",
+                    value: "15.956855",
+                    description: "纬度，例如：15.956855",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{ret:"1",errcode:"1"}',
+                description: 'errcode : 返回错误码,</br>ret : 返回值，0-成功，非0-失败'
+            },
+            return_code: [
+                 {
+                     errcode: 1,
+                     msg: "调用接口出错",
+                 }
+            ]
+        },
+        {
+            notes: "删除地理位置",
+            path: "/lbs/del_pos",
+            http_method: "post",
+            format: "json",
+            description: "本接口用于删除当前帐号最后一次的更新的地理位置。",
+            parameters: [
+                {
+                    parameter: "uid",
+                    value: "562354",
+                    description: "用户唯一ID",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{ret:"1",errcode:"1"}',
+                description: 'errcode : 返回错误码,</br>ret : 返回值，0-成功，非0-失败'
+            },
+            return_code: [
+                 {
+                     errcode: 1,
+                     msg: "调用接口出错",
+                 }
+            ]
+        }
+    ]
+});
---
+++ api/api/user.js
@@ -0,0 +1,119 @@
+﻿resources.push({
+    name: "USER",
+    operations: [
+        {
+            notes: "用户注册",
+            path: "/user/register",
+            http_method: "post",
+            format: "json",
+            description: "提交用户注册信息",
+            parameters: [
+                {
+                    parameter: "username",
+                    value: "peter",
+                    description: "用户名字",
+                    data_type: "string"
+                },
+                {
+                    parameter: "password",
+                    value: "peter222",
+                    description: "用户密码",
+                    data_type: "string"
+                },
+                {
+                    parameter: "mobile",
+                    value: "13804128888",
+                    description: "手机号码",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{code:"1",errcode:"1"}',
+                description: 'errcode : 返回错误码,</br>code : 返回值，0-成功，非0-失败'
+            },
+            return_code: [
+                 {
+                     errcode: 10001,
+                     msg: "用户名已被使用",
+                 },
+                 {
+                     errcode: 10002,
+                     msg: "手机号码已被使用",
+                 },
+                 {
+                     errcode: 10003,
+                     msg: "邮箱已被使用",
+                 },
+                 {
+                     errcode: 10004,
+                     msg: "帐户为空",
+                 },
+                 {
+                     errcode: 10005,
+                     msg: "密码为空",
+                 },
+                 {
+                     errcode: 10005,
+                     msg: "帐户名或密码出错",
+                 }
+            ]
+        },
+        {
+            notes: "用户登录",
+            path: "/user/login",
+            http_method: "post",
+            format: "json",
+            description: "用户登录信息接口",
+            parameters: [
+                {
+                    parameter: "mobile/email/account",
+                    value: "13804128888",
+                    description: "手机/邮箱/账号名",
+                    data_type: "string"
+                },
+                {
+                    parameter: "password",
+                    value: "123456",
+                    description: "密码",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{code:"1","tokenid": "dfc7ad3884e7",errcode:"10001"}',
+                description: 'errcode : 返回错误码,</br>code : 返回值，0-成功，非0-失败'
+            },
+            return_code: [
+                 {
+                     errcode: 10001,
+                     msg: "用户名不存在",
+                 },
+                 {
+                     errcode: 10001,
+                     msg: "密码错误",
+                 }
+            ]
+        },
+        {
+            notes: "用户退出",
+            path: "/user/logout",
+            http_method: "post",
+            format: "json",
+            description: "用户退出/注销操作接口",
+            parameters: [
+                {
+                    parameter: "tokenid",
+                    value: "dfc7ad3884e7",
+                    description: "token id",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{code:"1"}',
+                description: 'code : 返回值，0退出失败,1退出成功'
+            },
+            return_code: [
+          
+            ]
+        }
+    ]
+});
