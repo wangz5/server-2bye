c9c61b7da8ae33aadfd78c335add21116f0e9c4c
sycs <sycs@qq.com>
2013-07-01 13:51:06 +0800

    add

---
+++ api/api/register.js
@@ -0,0 +1,100 @@
+﻿resources.push({
+    name: "register",
+    operations: [
+        {
+            notes: "验证手机号码",
+            path: "/register/verify_mobile",
+            http_method: "post",
+            format: "json",
+            description: "验证手机号码",
+            parameters: [
+                {
+                    parameter: "mobile",
+                    value: '13804128888',
+                    value_type: 'text',
+                    mandatory:'yes',
+                    description: "手机号码",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{ret:1,errcode:10001}',
+                description: 'errcode : 返回错误码,</br>ret : 返回值，0 手机号码可以注册，1 手机号码无法注册'
+            },
+            return_code: [
+                 {
+                     errcode: 10001,
+                     msg: "手机号码已经被使用",
+                 },
+                 {
+                     errcode: 10002,
+                     msg: "手机号码已被禁用",
+                 }
+            ]
+        },//register
+        {
+            notes: "校对手机验证码",
+            path: "/register/verify_mobile_code",
+            http_method: "post",
+            format: "json",
+            description: "验证手机号码",
+            parameters: [
+                {
+                    parameter: "code",
+                    value: '13804128888',
+                    value_type: 'text',
+                    mandatory:'yes',
+                    description: "验证码",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{ret:1,errcode:10001}',
+                description: 'errcode : 返回错误码,</br>ret : 返回值，0 验证码正确，1 验证码错误'
+            },
+            return_code: [
+                 
+            ]
+        },//register
+        {
+            notes: "用户注册",
+            path: "/register/add_user",
+            http_method: "post",
+            format: "json",
+            description: "提交用户注册信息",
+            parameters: [
+                {
+                    parameter: "info",
+                    value: '{mobile:"13804128888",password:"peter222",birthday:"1984/08/08",usertype:"business"}',
+                    value_type: 'textarea',
+                    mandatory:'yes',
+                    description: "用户注册基本资料",
+                    data_type: "json"
+                }
+            ],
+            response: {
+                data: '{ret:1,errcode:10001}',
+                description: 'errcode : 返回错误码,</br>ret : 返回值，0-成功，1-失败'
+            },
+            return_code: [
+                 {
+                     errcode: 10001,
+                     msg: "用户名已被使用",
+                 },
+                 {
+                     errcode: 10002,
+                     msg: "帐户为空",
+                 },
+                 {
+                     errcode: 10003,
+                     msg: "密码为空",
+                 },
+                 {
+                     errcode: 10004,
+                     msg: "帐户名或密码出错",
+                 }
+            ]
+        }//register
+        
+    ]
+});
--- api/api/user.js
+++ api/api/user.js
@@ -2,61 +2,6 @@
     name: "user",
     operations: [
         {
-            notes: "用户注册",
-            path: "/user/register",
-            http_method: "post",
-            format: "json",
-            description: "提交用户注册信息",
-            parameters: [
-                {
-                    parameter: "info",
-                    value: '{mobile:"13804128888",password:"peter222",birthday:"1984/08/08",usertype:"business"}',
-                    value_type: 'textarea',
-                    mandatory:'yes',
-                    description: "用户注册基本资料",
-                    data_type: "json"
-                },
-                {
-                    parameter: "picture",
-                    value: "",
-                    value_type: "file",
-                    mandatory: 'yes',
-                    description: "form-data中媒体文件标识，有filename、filelength、content-type等信息",
-                    data_type: "bytes"
-                }
-            ],
-            response: {
-                data: '{ret:"1",errcode:"1"}',
-                description: 'errcode : 返回错误码,</br>ret : 返回值，0-成功，非0-失败'
-            },
-            return_code: [
-                 {
-                     errcode: 10001,
-                     msg: "用户名已被使用",
-                 },
-                 {
-                     errcode: 10002,
-                     msg: "手机号码已被使用",
-                 },
-                 {
-                     errcode: 10003,
-                     msg: "邮箱已被使用",
-                 },
-                 {
-                     errcode: 10004,
-                     msg: "帐户为空",
-                 },
-                 {
-                     errcode: 10005,
-                     msg: "密码为空",
-                 },
-                 {
-                     errcode: 10005,
-                     msg: "帐户名或密码出错",
-                 }
-            ]
-        },
-        {
             notes: "用户登录",
             path: "/user/login",
             http_method: "post",
@@ -90,11 +35,11 @@
                      msg: "用户名不存在",
                  },
                  {
-                     errcode: 10001,
+                     errcode: 10002,
                      msg: "密码错误",
                  }
             ]
-        },
+        },//login
         {
             notes: "用户退出",
             path: "/user/logout",
@@ -118,6 +63,45 @@
             return_code: [
           
             ]
-        }
+        },//logout
+        {
+            notes: "上传头像",
+            path: "/user/upload_image",
+            http_method: "post",
+            format: "json",
+            description: "上传用户头像接口",
+            parameters: [
+                {
+                    parameter: "pic",
+                    value: "",
+                    value_type: "file",
+                    mandatory: 'yes',
+                    description: "要上传的图片，仅支持JPEG、GIF、PNG格式，图片大小小于5M",
+                    data_type: "binary"
+                },
+                {
+                    parameter: "tokenid",
+                    value: "dfc7ad3884e7",
+                    value_type: "text",
+                    mandatory: 'yes',
+                    description: "登录时服务端返回的tokenid",
+                    data_type: "string"
+                }
+            ],
+            response: {
+                data: '{ ret:0 }',
+                description: 'ret : 返回值，0 上传成功，1 上传失败'
+            },
+            return_code: [
+                 {
+                     errcode: 10001,
+                     msg: "上传成功",
+                 },
+                 {
+                     errcode: 10002,
+                     msg: "上传失败",
+                 }
+            ]
+        }//upload_image
     ]
 });
